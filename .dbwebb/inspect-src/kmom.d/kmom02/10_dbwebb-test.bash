207512fdf6ba99de329fbf3e5ac3bcbdae86ec23
Kenneth Lewenhagen <lewenhagen@live.com>
2023-11-17 09:17:17 +0100

    774822

--- .dbwebb/inspect-src/kmom.d/kmom02/10_dbwebb-test.bash
+++ .dbwebb/inspect-src/kmom.d/kmom02/10_dbwebb-test.bash
@@ -4,7 +4,7 @@ function header {
     printf "\033[32;01m>>> -------------- %-20s -------------------------\033[0m\n" "$1"
 }
 
-dbwebb test kmom02 || exit 1
+dbwebb test kmom02
 
 printf "\n%s" "Press any key to proceed."
 read
