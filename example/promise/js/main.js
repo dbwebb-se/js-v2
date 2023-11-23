0ae57852a4e55866eb15b483af05cbd7dad76153
lewenhagen <lewenhagen@live.com>
2023-11-23 14:02:59 +0100

    fixed promise example

--- example/promise/js/main.js
+++ example/promise/js/main.js
@@ -1,25 +1,21 @@
-(function () {
-    'use strict';
+function promise1 () {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => resolve('42'), 3000)
+    })
+}
 
-    function promise1 () {
-        return new Promise((resolve, reject) => {
-            setTimeout(() => resolve('42'), 3000);
-        });
-    }
+function promise2 () {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => resolve('done'), 2000)
+    })
+}
 
-    function promise2 () {
-        return new Promise((resolve, reject) => {
-            setTimeout(() => resolve('done'), 2000);
-        });
-    }
+const promise3 = new Promise((resolve, reject) => {
+    setTimeout(resolve, 100, "even me")
+})
 
-    const promise3 = new Promise((resolve, reject) => {
-        setTimeout(resolve, 100, "even me");
-    });
+promise1().then((result) => console.log(result))
 
-    promise1().then((result) => console.log(result));
-
-    Promise.all([promise1(), promise2(), promise3]).then((results) => {
-        console.log(results);
-    });
-})();
+Promise.all([promise1(), promise2(), promise3]).then((results) => {
+    console.log(results)
+})
