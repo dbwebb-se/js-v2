1aef64460b66cd639161ba3f09905607f4b78e30
lewenhagen <lewenhagen@live.com>
2023-11-23 14:42:45 +0100

    changed localstorage to sessionstorage

--- components/05/02_web-storage-api.md
+++ components/05/02_web-storage-api.md
@@ -3,7 +3,7 @@ Web Storage API
 
 JavaScript på klientsidan kan lagra data i webbläsaren åt oss. Datan lagras som strängar i nyckel/värde-par. Det är heller ingen data som skickas till någon server, utan den stannar i webbläsaren. Numer finns det hela applikationer som körs med hjälp av Web Storage.
 
-Windowobjektet ger oss två sätt att lagra datan på: *sessionStorage* och *localStorage*.
+Windowobjektet ger oss två sätt att lagra datan på: *sessionStorage* och *sessionStorage*.
 
 Läs mer om [Web Storage på mdn](https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API)
 
@@ -15,9 +15,9 @@ När vi sparar i sessionen lagras datan så länge som webbläsaren är öppen, 
 
 
 
-#### localStorage
+#### sessionStorage
 
-I localStorage lagras datan under en längre tid och man kan bara ta bort datan via JavaScript eller en rensning av cachen eller lokalt lagrad data.
+I sessionStorage lagras datan under en längre tid och man kan bara ta bort datan via JavaScript eller en rensning av cachen eller lokalt lagrad data.
 
 
 
@@ -27,12 +27,12 @@ Oavsett vilken mekanism vi använder för att lagra datan har vi tillgång till 
 
 ```js
 // Set items
-window.localStorage.setItem('name', 'Murphy')
-window.localStorage.profession = `Stormtrooper`
+window.sessionStorage.setItem('name', 'Murphy')
+window.sessionStorage.profession = `Stormtrooper`
 
 // Get items
-const name = window.localStorage.getItem('name')
-const profession = window.localStorage.profession
+const name = window.sessionStorage.getItem('name')
+const profession = window.sessionStorage.profession
 ```
 
 Vi kan se att det finns två funktioner som hjälper oss men vi kan även peta på det med nyckel/värde.
@@ -40,7 +40,7 @@ Vi kan se att det finns två funktioner som hjälper oss men vi kan även peta p
 Behöver vi radera datan finns även där en funktion till hjälp:
 
 ```js
-let myStore = window.localStorage
+let myStore = window.sessionStorage
 
 myStore.setItem('tomato', "45")
 
--- example/webstorage/js/main.js
+++ example/webstorage/js/main.js
@@ -1,43 +1,39 @@
-(function () {
-    'use strict';
+const incBtn = document.getElementById('inc')
+const decBtn = document.getElementById('dec')
+const clearBtn = document.getElementById('clear')
+const result = document.getElementById('result')
 
-    const incBtn = document.getElementById('inc');
-    const decBtn = document.getElementById('dec');
-    const clearBtn = document.getElementById('clear');
-    const result = document.getElementById('result');
+result.innerHTML = parseInt(sessionStorage.getItem('value')) || 0
 
-    result.innerHTML = parseInt(localStorage.getItem('value')) || 0;
+function dumpStorage () {
+    console.log(sessionStorage)
+}
 
-    function dumpStorage () {
-        console.log(localStorage);
-    }
+function increaseValue () {
+    const old = parseInt(result.innerHTML)
 
-    function increaseValue () {
-        const old = parseInt(result.innerHTML);
+    result.innerHTML = old + 1
+}
 
-        result.innerHTML = old + 1;
-    }
+function decreaseValue () {
+    const old = parseInt(result.innerHTML)
 
-    function decreaseValue () {
-        const old = parseInt(result.innerHTML);
+    result.innerHTML = old - 1
+}
 
-        result.innerHTML = old - 1;
-    }
+incBtn.addEventListener('click', increaseValue)
 
-    incBtn.addEventListener('click', increaseValue);
+decBtn.addEventListener('click', decreaseValue)
 
-    decBtn.addEventListener('click', decreaseValue);
+clearBtn.addEventListener('click', function () {
+    sessionStorage.clear()
+    dumpStorage()
+    result.innerHTML = 0
+})
 
-    clearBtn.addEventListener('click', function () {
-        localStorage.clear();
-        dumpStorage();
-        result.innerHTML = 0;
-    });
+window.addEventListener('unload', function () {
+    sessionStorage.setItem('value', result.innerHTML)
+})
 
-    window.addEventListener('unload', function () {
-        localStorage.setItem('value', result.innerHTML);
-    });
-
-    dumpStorage();
-    window.console.log('Sandbox is ready!');
-})();
+dumpStorage()
+window.console.log('Sandbox is ready!')
