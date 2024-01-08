1dc8fe21c7c1f309586b6d96d2b803c3819d9a0c
lewenhagen <lewenhagen@live.com>
2024-01-08 10:43:03 +0100

    8748392

--- .dbwebb/inspect-src/kmom.d/kmom10/pre.bash
+++ .dbwebb/inspect-src/kmom.d/kmom10/pre.bash
@@ -1,19 +1,10 @@
 #!/usr/bin/env bash
 
-red=$(tput setaf 1)
-green=$(tput setaf 2)
-cyan=$(tput setaf 6)
-normal=$(tput sgr 0)
-
 function header {
     printf "\033[32;01m>>> -------------- %-20s -------------------------\033[0m\n" "$1"
 }
 
 cd me || exit 1
 
-header "OPENING FILES IN ATOM"
-code kmom10
-
-header "OPEN ASSIGNMENT IN BROWSER"
-url="$REDOVISA_HTTP_PREFIX/~$ACRONYM/dbwebb-kurser/$COURSE/me/$KMOM"
-eval "$BROWSER" "$url" &
+header "OPENING FILES IN VSCode"
+code public
